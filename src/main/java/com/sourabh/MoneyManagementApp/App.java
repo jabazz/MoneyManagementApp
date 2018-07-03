a49e009f84e06a0665b6cdc8afe0bfb1e88faba8
Sourabh Kumawat <sourabhkumawat94@gmail.com>
2018-07-03 18:16:54 +0530

    added partial code for individual different contribution.

--- src/main/java/com/sourabh/MoneyManagementApp/App.java
+++ src/main/java/com/sourabh/MoneyManagementApp/App.java
@@ -10,7 +10,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        RetriveOprations.individualBalance("raja");
+        public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Map<String ,Double> contri = new HashMap<>();
+		String contributer = null;
+		Double contribution;
+		while(!"done".equals(contributer)){
+			System.out.println("Enter the name of the contributer or Enter done if you are done");
+			 contributer = sc.next();
+			 if(contributer.equals("done")){break;}
+			 System.out.println("Enter the amount contributed");
+			 contribution =sc.nextDouble();
+			 contri.put(contributer, contribution);
+		}
+		System.out.println("Money Contributed:-->"+contri);
+		System.out.println("Individual payable amount--->"+ indidualAmountPayable(contri));
+		System.out.println("Individual Banlance--->"+ individualBalance(contri,indidualAmountPayable(contri)));
+	}
+	private static double indidualAmountPayable(Map<String, Double> contri){
+		double value = 0.0;
+		for (Entry<String, Double> entry : contri.entrySet()){
+             value +=  entry.getValue();
+            }
+		System.out.println(value+"/"+contri.size());
+		return value/contri.size();
+		}
+	private static Map individualBalance(Map<String, Double > m, double value){
+		for (Map.Entry<String,Double> entry : m.entrySet()){
+             entry.setValue(value - entry.getValue());
+             }
+		return m;
+}
         
     }
 }
